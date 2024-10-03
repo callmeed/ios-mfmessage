e2eb52a5620fc3a7406c4d1e898cde0b7bd52c59
edungan <edungan@indeed.com>
2024-10-03 09:49:46 -0700

    Initial Commit

---
+++ ComposeTest/ComposeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposeTestApp.swift
+//  ComposeTest
+//
+//  Created by Ed Dungan on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
